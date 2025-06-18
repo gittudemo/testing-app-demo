9ad7ef24e791035bf0d2be354681a56ab57437f2
qadir <aqadir1989@gmail.com>
2025-06-18 19:47:37 +0530

    using-spyon() on console.log-method-not actually-creating -object

---
+++ src/app/services/logger.service.spec.ts
@@ -0,0 +1,15 @@
+import { LoggerService } from "./logger.service";
+
+describe("logger service", () => {
+
+  it("should log messages", () => {
+    const logger = new LoggerService();
+    const consoleSpy = spyOn(console, 'log');
+    
+    logger.log("Test message");
+    
+    expect(consoleSpy).toHaveBeenCalledWith("LOGGER LOG:Test message");
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+    expect(consoleSpy.calls.first().args[0]).toBe("LOGGER LOG:Test message");
+  });
+});
