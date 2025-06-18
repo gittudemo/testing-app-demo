767827274bf174f97c923d51f9fbfb1a35b390c6
qadir <aqadir1989@gmail.com>
2025-06-18 18:42:51 +0530

    Basic-project-for-posting-the-data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommentComponent } from './components/comment/comment.component';
+import { UserComponent } from './components/user/user.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,NavComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testing-app-demo';
+  title = 'test-new-prep';
 }
